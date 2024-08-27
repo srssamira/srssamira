7b4dc02867faddad6f3f8afc6a6170bc63fd7470
Samira Rodrigues <samira.silva@zup.com.br>
2024-08-27 16:41:25 -0300

    add corrections in sumScore method

--- src/ManipulationSlotMachine.java
+++ src/ManipulationSlotMachine.java
@@ -4,43 +4,35 @@ public class ManipulationSlotMachine {
 
     public void sortValues(int difficulty) {
 
-        ContentSlotMachine[] sortSlotMachine = ContentSlotMachine.values();
+        ContentSlotMachine[] sortSlotMachine = new ContentSlotMachine[3];
 
         int sumSortSlotMachine = 0;
         for (int index = 0; index < difficulty; index++) {
-            for (int subIndex = 0; subIndex < 3; subIndex++) {
-                int random = (int) (Math.random() * 3);
+            for (int subIndex = 0; subIndex < slotMachine.length; subIndex++) {
+                int random = (int) (Math.random() * slotMachine.length);
                 sortSlotMachine[subIndex] = slotMachine[random];
                 System.out.println(sortSlotMachine[subIndex].getName());
-                sumSortSlotMachine = sumScore(sortSlotMachine, difficulty);
             }
+            sumSortSlotMachine += sumScore(sortSlotMachine, difficulty);
         }
         System.out.println(sumSortSlotMachine + " PONTOS");
     }
 
     public int sumScore(ContentSlotMachine[] slotMachine, int difficulty) {
         int sum = 0;
-        for (int index = 0; index < difficulty; index++) {
-            for (int subIndex = 0; subIndex < 3; subIndex++) {
-                sum = sum + slotMachine[subIndex].getScore();
-            }
+        for (int index = 0; index < slotMachine.length; index++) {
+            sum += slotMachine[index].getScore();
         }
         return sum;
     }
 
-    public int addBonus(ContentSlotMachine[] slotMachine, int difficulty) {
-        int sumAddBonus = 0;
-
-        for (int index = 0; index < difficulty; index++) {
-            for (int subIndex = 0; subIndex < 3; subIndex++) {
-                if ((slotMachine[0].getName().equalsIgnoreCase(slotMachine[1].getName()))
-                        && (slotMachine[1].getName().equalsIgnoreCase(slotMachine[2].getName()))) {
-                    sumAddBonus = (sumScore(slotMachine, difficulty)) * 100;
-                }
-            }
-        }
-
-        return sumAddBonus;
-    }
+//    public int addBonus(ContentSlotMachine[] slotMachine, int difficulty) {
+//        int sumAddBonus = 0;
+//        if ((slotMachine[0].getName().equalsIgnoreCase(slotMachine[1].getName()))
+//                && (slotMachine[1].getName().equalsIgnoreCase(slotMachine[2].getName()))) {
+//            sumAddBonus = (sumScore(slotMachine, difficulty)) * 100;
+//        }
+//        return sumAddBonus;
+//    }
 }
 
